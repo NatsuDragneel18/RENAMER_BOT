da136690cf9120913ffc088841dfde2233ebf889
NatsuDragneel18 <88961572+NatsuDragneel18@users.noreply.github.com>
2021-09-20 17:09:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,8 +12,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Anime Channel" ,url="https://t.me/AnimeChamberUploads") ], 
+	[InlineKeyboardButton("Manga Channel", url="https://t.me/thechamberofanimefans") ]  ]))
 
 
 
